7bec775f437a4b43663acf5f070e1c02d85da7bc
Luca Garizio <garizioluca2@gmail.com>
2024-02-13 18:55:27 +0100

    add query where on controller to check just the train that depart today

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -5,12 +5,16 @@ use Illuminate\Http\Request;
 
 use App\Models\Train;
 
+use Illuminate\Support\Carbon;
+
 class MainController extends Controller
 {
-    
-    public function index() {
+    public function index()
+    {
+        
+        $today = Carbon::today();
 
-        $trains = Train :: all();
+        $trains = Train::whereDate('orario_di_partenza', $today)->get();
 
         return view('pages.index', compact('trains'));
     }
